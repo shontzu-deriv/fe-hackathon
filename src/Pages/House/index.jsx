81acaaba0887615c00965429034b6ebdb6d49396
shontzu <shon.tzu@besquare.com.my>
2022-07-18 18:21:34 +0800

    fetch house

--- src/Pages/House/index.jsx
+++ src/Pages/House/index.jsx
@@ -1,5 +1,42 @@
+import React, { useEffect } from "react";
+import { getHouse } from "../../fetch";
+
 const House = () => {
-  return <div>Rumah besar</div>;
+  const [house, changeHouse] = React.useState([]);
+
+  // wrap fetch in useEffect watch empty array (triggers on Mount)
+  // useEffect(() => {
+  //   getHouse()
+  // }, []);
+
+  const setHouse = (param) => {
+    getHouse(param).then(changeHouse);
+  };
+
+  return (
+    <div>
+      <button onClick={() => setHouse("gryffindor")}>Gryffindor</button>
+      <button onClick={() => setHouse("ravenclaw")}>Ravenclaw</button>
+      <button onClick={() => setHouse("hufflepuff")}>Hufflepuff</button>
+      <button onClick={() => setHouse("slytherin")}>Slytherin</button>
+
+      <div>
+        {house.map(function (key, index) {
+          return (
+            <div key={index}>
+              <b>name: </b>
+              {key.name}
+              <br />
+              <b>dob: </b>
+              {key.dateOfBirth}
+              <br />
+              <hr />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default House;
--- src/Pages/Staffs/index.jsx
+++ src/Pages/Staffs/index.jsx
@@ -1,46 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { getStaff } from "../../fetch";
 
 const Staffs = () => {
   const [staff, setStaff] = React.useState([]);
 
-  //connect to resource
-  //TODO: wrap fetch in useEffect watch empty array (meaning, triggersgit onMount)
-  // useEffect(()=>{
-  async function getStaff() {
-    await fetch("http://hp-api.herokuapp.com/api/characters/staff", {
-      method: "GET",
-    })
-      .then((response) => {
-        console.log(response);
-        return response.json();
-      })
-      .then((json) => {
-        console.log(json);
-        return setStaff(json);
-      });
-  }
-  // },[])
+  // wrap fetch in useEffect watch empty array (triggers on Mount)
+  useEffect(() => {
+    getStaff().then(setStaff)
+  }, []);
 
   return (
     <div>
-      <button onClick={getStaff}>getStaff</button>
-      <div>
-        {staff.map(function (key, index) {
-          return (
-            <div key={index}>
-              <b>name: </b>
-              {key.name}
-              <br />
-              <b>dob: </b>
-              {key.dateOfBirth}
-              <br />
-              <b>house: </b>
-              {key.house}
-              <hr />
-            </div>
-          );
-        })}
-      </div>
+      {staff.map(function (key, index) {
+        return (
+          <div key={index}>
+            <b>name: </b>
+            {key.name}
+            <br />
+            <b>dob: </b>
+            {key.dateOfBirth}
+            <br />
+            <b>house: </b>
+            {key.house}
+            <hr />
+          </div>
+        );
+      })}
     </div>
   );
 };
