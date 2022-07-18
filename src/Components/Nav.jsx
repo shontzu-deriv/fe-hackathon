c1865a2f7b58066602c23fd252d9551007d28c24
shontzu <shon.tzu@besquare.com.my>
2022-07-18 15:36:03 +0800

    reorganize components

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import About_Us from "../Pages/About_Us";
+import AboutUs from "../Pages/AboutUs";
 import House from "../Pages/House";
 import Staffs from "../Pages/Staffs";
 import Students from "../Pages/Students";
@@ -9,12 +9,12 @@ export const Nav = () => {
   return (
     <div>
       <BrowserRouter>
-        <Link to="/About_Us">About Us</Link>
+        <Link to="/AboutUs">About Us</Link>
         <Link to="/House">House</Link>
         <Link to="/Staffs">Staffs</Link>
         <Link to="/Students">Students</Link>
         <Routes>
-          <Route path="About_Us" element={<About_Us />} />
+          <Route path="AboutUs" element={<AboutUs />} />
           <Route path="House" element={<House />} />
           <Route path="Staffs" element={<Staffs />} />
           <Route path="Students" element={<Students />} />
