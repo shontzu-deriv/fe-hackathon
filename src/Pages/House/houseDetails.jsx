acdc613291a3f4a5f39f2e47a1e0e271a9c52aaa
shontzu alias yuuchin <shon.tzu@besquare.com.my>
2022-07-19 17:23:34 +0800

    fix rerender UI bug at house component

--- src/Pages/House/houseDetails.jsx
+++ src/Pages/House/houseDetails.jsx
@@ -5,12 +5,14 @@ import { getHouse } from "../../fetch";
 export const HouseDetails = () => {
   const [houseDetails, setHouseDetails] = React.useState([]);
 
+  const { house } = useParams();
   // wrap fetch in useEffect watch empty array (triggers on Mount)
   useEffect(() => {
+    // prop updated but didn't trigger onMount, need to pass the prop into the watch array to rerender UI
+    setHouseDetails([]);
     getHouse(house).then(setHouseDetails);
-  }, []);
+  }, [house]);
 
-  const { house } = useParams();
   return (
     <div>
       <h1>{house}</h1>
