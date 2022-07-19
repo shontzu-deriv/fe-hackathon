1b5b939af4aaf3433eaf0b491094ccd15873b4ee
shontzu alias yuuchin <shon.tzu@besquare.com.my>
2022-07-19 14:47:16 +0800

    if-else render img

--- src/Pages/Staffs/index.jsx
+++ src/Pages/Staffs/index.jsx
@@ -13,22 +13,24 @@ const Staffs = () => {
   return (
     <div className="main-container">
       <div>
-        {staff.filter(t=>t.image&&true).map(function (key, index) {
-          return (
-            <div
-              className="card"
-              key={index}
-              onClick={() => {
-                alert(`TEST: ${key.name}`);
-              }}
-            >
-              <img src={key.image} alt=""/>
-              {/* <i>{key.image||"no image"}</i> */}
-              <span>{key.name}</span>
-              <span>{'>'}</span>
-            </div>
-          );
-        })}
+        {staff
+          // .filter((t) => t.image && true)
+          .map(function (key, index) {
+            return (
+              <div
+                className="card"
+                key={index}
+                onClick={() => {
+                  alert(`TEST: ${key.name}`);
+                }}
+              >
+                {/* {!key.image ? <> : <>} */}
+                <img src={key.image !== "" ? key.image : "https://www.seekpng.com/png/detail/966-9665317_placeholder-image-person-jpg.png"} alt=""/>
+                <span>{key.name}</span>
+                <span>{">"}</span>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
