dfa7cb1c0293ae13e728194eefd30a209d338ebd
Derrick <derrick@besquare.com.my>
2022-07-20 11:28:28 +0800

    editing homepage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import Nav from "./Components/Nav.jsx";
 import Footer from "./Components/Footer";
 import "./fetch";
@@ -8,12 +7,17 @@ import HouseDetails from "./Pages/House/houseDetails";
 import Staffs from "./Pages/Staffs";
 import Students from "./Pages/Students";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import Header from "./Components/Header.jsx";
+import Home from "./Pages/Home.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Nav />
+
       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/app" element={<Home />} />
         <Route path="aboutus" element={<AboutUs />} />
         <Route path="/house" element={<House />}>
           <Route path=":house" element={<HouseDetails />} />
---
+++ src/Components/title.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./header.css";
+const Showcase = () => {
+  return (
+    <section className="showcase">
+      <div className="showcase-overlay">
+        <h1> LET THE MAGIC BEGIN </h1>
+        <p>
+          <b>IN HOGWARTS</b>
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Showcase;
