3ad80db63d950ac3385cb6fe8a2acacd3619e9cc
Derrick <derrick@besquare.com.my>
2022-07-21 22:48:13 +0800

    editing the css for the 2nd section in about us page

--- src/Pages/AboutUs/About.jsx
+++ src/Pages/AboutUs/About.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./index.jsx";
 import AboutUsIndex from "./index.jsx";
+import Review from "./section2.jsx";
 
 const About = () => {
   return (
     <section>
       <AboutUsIndex />
+      <Review />
     </section>
   );
 };
--- src/Pages/AboutUs/section2.jsx
+++ src/Pages/AboutUs/section2.jsx
@@ -2,141 +2,157 @@ import "./secstyle.css";
 
 const Review = () => {
   return (
-
     <section id="testimonials">
-        {/* heading */}
-        <div class="testimonial-heading">
-            <span>Comments</span>
-            <h4>Clients Says</h4>
+      {/* heading */}
+      <div className="testimonial-heading">
+        <span>Comments</span>
+        <h4>Clients Says</h4>
+      </div>
+      {/* <!--testimonials-box-container------> */}
+      <div className="testimonial-box-container">
+        {/* <!--BOX-1--------------> */}
+        <div className="testimonial-box">
+          {/* <!--top-------------------------> */}
+          <div className="box-top">
+            {/* <!--profile-----> */}
+            <div className="profile">
+              {/* <!--img----> */}
+              <div className="profile-img">
+                <img src="https://cdn3.iconfinder.com/data/icons/avatars-15/64/_Ninja-2-512.png" />
+              </div>
+              {/* <!--name-and-username--> */}
+              <div className="name-user">
+                <strong>Liam mendes</strong>
+                <span>@liammendes</span>
+              </div>
+            </div>
+            {/* <!--reviews------> */}
+            <div className="reviews">
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="far fa-star"></i>
+            </div>
+          </div>
+          {/* <!--Comments----------------------------------------> */}
+          <div className="client-comment">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Exercitationem, quaerat quis? Provident temporibus architecto
+              asperiores nobis maiores nisi a. Quae doloribus ipsum aliquam
+              tenetur voluptates incidunt blanditiis sed atque cumque.
+            </p>
+          </div>
         </div>
-        {/* <!--testimonials-box-container------> */}
-        <div class="testimonial-box-container">
-            {/* <!--BOX-1--------------> */}
-            <div class="testimonial-box">
-                {/* <!--top-------------------------> */}
-                <div class="box-top">
-                    {/* <!--profile-----> */}
-                    <div class="profile">
-                        {/* <!--img----> */}
-                        <div class="profile-img">
-                            <img src="https://cdn3.iconfinder.com/data/icons/avatars-15/64/_Ninja-2-512.png" />
-                        </div>
-                        {/* <!--name-and-username--> */}
-                        <div class="name-user">
-                            <strong>Liam mendes</strong>
-                            <span>@liammendes</span>
-                        </div>
-                    </div>
-                    {/* <!--reviews------> */}
-                    <div class="reviews">
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="far fa-star"></i><!--Empty star-->
-                    </div>
-                </div>
-                {/* <!--Comments----------------------------------------> */}
-                <div class="client-comment">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, quaerat quis? Provident temporibus architecto asperiores nobis maiores nisi a. Quae doloribus ipsum aliquam tenetur voluptates incidunt blanditiis sed atque cumque.</p>
-                </div>
+        {/* <!--BOX-2--------------> */}
+        <div className="testimonial-box">
+          {/* <!--top-------------------------> */}
+          <div className="box-top">
+            {/* <!--profile-----> */}
+            <div className="profile">
+              {/* <!--img----> */}
+              <div className="profile-img">
+                <img src="https://cdn3.iconfinder.com/data/icons/avatars-15/64/_Ninja-2-512.png" />
+              </div>
+              {/* <!--name-and-username--> */}
+              <div className="name-user">
+                <strong>Noah Wood</strong>
+                <span>@noahwood</span>
+              </div>
             </div>
-            {/* <!--BOX-2--------------> */}
-            <div class="testimonial-box">
-                {/* <!--top-------------------------> */}
-                <div class="box-top">
-                    {/* <!--profile-----> */}
-                    <div class="profile">
-                        {/* <!--img----> */}
-                        <div class="profile-img">
-                            <img src="https://cdn3.iconfinder.com/data/icons/avatars-15/64/_Ninja-2-512.png" />
-                        </div>
-                        {/* <!--name-and-username--> */}
-                        <div class="name-user">
-                            <strong>Noah Wood</strong>
-                            <span>@noahwood</span>
-                        </div>
-                    </div>
-                    {/* <!--reviews------> */}
-                    <div class="reviews">
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i><!--Empty star-->
-                    </div>
-                </div>
-                {/* <!--Comments----------------------------------------> */}
-                <div class="client-comment">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, quaerat quis? Provident temporibus architecto asperiores nobis maiores nisi a. Quae doloribus ipsum aliquam tenetur voluptates incidunt blanditiis sed atque cumque.</p>
-                </div>
+            {/* <!--reviews------> */}
+            <div className="reviews">
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
             </div>
-            {/* <!--BOX-3--------------> */}
-            <div class="testimonial-box">
-                {/* <!--top-------------------------> */}
-                <div class="box-top">
-                    {/* <!--profile-----> */}
-                    <div class="profile">
-                        {/* <!--img----> */}
-                        <div class="profile-img">
-                            <img src="https://cdn3.iconfinder.com/data/icons/avatars-15/64/_Ninja-2-512.png" />
-                        </div>
-                        {/* <!--name-and-username--> */}
-                        <div class="name-user">
-                            <strong>Oliver Queen</strong>
-                            <span>@oliverqueen</span>
-                        </div>
-                    </div>
-                    {/* <!--reviews------> */}
-                    <div class="reviews">
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="far fa-star"></i> 
-                    </div>
-                </div>
-                {/* <!--Comments----------------------------------------> */}
-                <div class="client-comment">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, quaerat quis? Provident temporibus architecto asperiores nobis maiores nisi a. Quae doloribus ipsum aliquam tenetur voluptates incidunt blanditiis sed atque cumque.</p>
-                </div>
-            </div>
-            {/* <!--BOX-4--------------> */}
-            <div class="testimonial-box">
-                {/* <!--top-------------------------> */}
-                <div class="box-top">
-                    {/* <!--profile-----> */}
-                    <div class="profile">
-                        {/* <!--img----> */}
-                        <div class="profile-img">
-                            <img src="https://cdn3.iconfinder.com/data/icons/avatars-15/64/_Ninja-2-512.png" />
-                        </div>
-                        {/* <!--name-and-username--> */}
-                        <div class="name-user">
-                            <strong>Barry Allen</strong>
-                            <span>@barryallen</span>
-                        </div>
-                    </div>
-                    {/* <!--reviews------> */}
-                    <div class="reviews">
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="far fa-star"></i>
-                    </div>
-                </div>
-                {/* <!--Comments----------------------------------------> */}
-                <div class="client-comment">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, quaerat quis? Provident temporibus architecto asperiores nobis maiores nisi a. Quae doloribus ipsum aliquam tenetur voluptates incidunt blanditiis sed atque cumque.</p>
-                </div>
-            </div>
+          </div>
+          {/* <!--Comments----------------------------------------> */}
+          <div className="client-comment">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Exercitationem, quaerat quis? Provident temporibus architecto
+              asperiores nobis maiores nisi a. Quae doloribus ipsum aliquam
+              tenetur voluptates incidunt blanditiis sed atque cumque.
+            </p>
+          </div>
         </div>
-      </section>
-
+        {/* <!--BOX-3--------------> */}
+        <div className="testimonial-box">
+          {/* <!--top-------------------------> */}
+          <div className="box-top">
+            {/* <!--profile-----> */}
+            <div className="profile">
+              {/* <!--img----> */}
+              <div className="profile-img">
+                <img src="https://cdn3.iconfinder.com/data/icons/avatars-15/64/_Ninja-2-512.png" />
+              </div>
+              {/* <!--name-and-username--> */}
+              <div className="name-user">
+                <strong>Oliver Queen</strong>
+                <span>@oliverqueen</span>
+              </div>
+            </div>
+            {/* <!--reviews------> */}
+            <div className="reviews">
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="far fa-star"></i>
+            </div>
+          </div>
+          {/* <!--Comments----------------------------------------> */}
+          <div className="client-comment">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Exercitationem, quaerat quis? Provident temporibus architecto
+              asperiores nobis maiores nisi a. Quae doloribus ipsum aliquam
+              tenetur voluptates incidunt blanditiis sed atque cumque.
+            </p>
+          </div>
+        </div>
+        {/* <!--BOX-4--------------> */}
+        <div className="testimonial-box">
+          {/* <!--top-------------------------> */}
+          <div className="box-top">
+            {/* <!--profile-----> */}
+            <div className="profile">
+              {/* <!--img----> */}
+              <div className="profile-img">
+                <img src="https://cdn3.iconfinder.com/data/icons/avatars-15/64/_Ninja-2-512.png" />
+              </div>
+              {/* <!--name-and-username--> */}
+              <div className="name-user">
+                <strong>Barry Allen</strong>
+                <span>@barryallen</span>
+              </div>
+            </div>
+            {/* <!--reviews------> */}
+            <div className="reviews">
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="far fa-star"></i>
+            </div>
+          </div>
+          {/* <!--Comments----------------------------------------> */}
+          <div className="client-comment">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Exercitationem, quaerat quis? Provident temporibus architecto
+              asperiores nobis maiores nisi a. Quae doloribus ipsum aliquam
+              tenetur voluptates incidunt blanditiis sed atque cumque.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   );
-}
-
-
+};
 
 export default Review;
