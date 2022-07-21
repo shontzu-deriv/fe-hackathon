bb1aa1c85cedcc1a0f9ac4cf3c72194220f0b8ec
Derrick <derrick@besquare.com.my>
2022-07-21 14:33:03 +0800

    edit media equery for body

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,13 @@
-// import "./header.css";
+import "./header.css";
 
-// export default function Header() {
-//   return (
-//     <div className="header">
-//       <div className="headerTitles">
-//         <span className="headerTitleSm">Let The Magic Begin</span>
-//         <span className="headerTitleLg">HOGWARTS</span>
-//       </div>
-//       <img className="headerImg" />
-//     </div>
-//   );
-// }
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="headerTitles">
+        <span className="headerTitleSm">Let The Magic Begin</span>
+        <span className="headerTitleLg">HOGWARTS</span>
+      </div>
+      <img className="headerImg" />
+    </div>
+  );
+}
